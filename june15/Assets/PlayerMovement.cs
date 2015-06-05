d3f1053ee310834862df848acbd12715c5f709c4
burlapjack <burlapjack@yahoo.com>
2015-06-05 16:04:59 -0500

    Initial Project Submission
    Nothing has been done yet in this initial submission besides basic
    3rd-person character control.

---
+++ june15/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed;
+	public float jumpSpeed;
+	private Rigidbody rb;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce(movement * speed);
+		if(
+			Input.GetButton("Jump"))
+			Jump();
+
+	}
+
+	void Jump()
+	{
+		rb.AddForce(Vector3.up * jumpSpeed);
+	}
+}
